a74225789337c6e0a4070f9a264f9667446855a1
puffleeck \ павлик <120432834+puffleeck@users.noreply.github.com>
2024-07-22 05:53:21 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,65 @@
+;var y = [{ // data
+parent: 'q',
+name: 'id',
+tag: 'div',
+clas: 'tabs gafd',
+elements: ['t_Canvas', 't_Changes'],
+	},{
+
+parent: 't_Changes',
+name: 'innerHTML',
+tag: 'div',
+clas: 'message5',
+style: 'grid-column: 1',
+elements: ["icon's", '^ upd', '* upg', '+ add', '- del', '/ div', '@ fix', '> replace', '-> merge',],
+	},{
+parent: 't_Changes',
+name: 'innerHTML',
+tag: 'div',
+clas: 'message3',
+style: 'grid-column: 2',
+elements: ['15.07.2022', '+ #sample', '^ #Print'],
+	},{
+parent: 't_Changes',
+name: 'innerHTML',
+tag: 'div',
+clas: 'message6',
+style: 'grid-column: 3',
+elements: ['31.08.2022', '+ #tpl', '+ $$.sort', '^ #f']
+	},{
+parent: 't_Changes',
+name: 'innerHTML',
+tag: 'div',
+clas: 'message4',
+style: 'grid-column: 4',
+elements: ['17.10.2023', '+ select float', '@ select inline', '@ #tpl css snippet', '@ #NoPrint']
+	},{
+parent: 't_Changes',
+name: 'innerHTML',
+tag: 'div',
+clas: 'message3',
+style: 'grid-column: 5',
+elements: ['19.10.2023', '+ .tabs:target', '@ #tpl', '@ $$.reStyler', '@ @media', 'sort() > CCN()']
+	},{
+parent: 't_Changes',
+name: 'innerHTML',
+tag: 'div',
+clas: 'message7',
+style: 'grid-column: 6',
+elements: ['25.10.2023', '+ Gantt', '+ display', '^ info', '@ new_add()', 'cards -> index']
+	},{
+parent: 't_Changes',
+name: 'innerHTML',
+tag: 'div',
+clas: 'message4',
+style: 'grid-column: 7',
+elements: ['28.12.2023', '+ switch lang`s', '* #cards', '@ custom', '/ *.js, *.css']
+	},{
+parent: 't_Changes',
+name: 'innerHTML',
+tag: 'div',
+clas: 'message3',
+style: 'grid-column: 8',
+elements: ['18.01.2024', '* #cards', '* #menu', '^ #info', "^ :hover's", '^ .snippet',],
+	}];
+for(let i =0; y[i]; i++){$$.creator(y[i]);}
